9d3e8f76e2aefcf4081ab91f501e17f5a4fd4e6b
Dave <davidlongswe@gmail.com>
2022-12-05 07:38:32 +0100

    parsed

--- 05/05.js
+++ 05/05.js
@@ -1,5 +1,30 @@
 const fs = require("fs");
 const input = fs
   .readFileSync("05.txt", { encoding: "utf-8" })
-  .split("\n")
+  .split("\r\n\r\n")
   .filter((x) => x);
+
+const stackInfo = input[0].split("\r\n").map((x) => {
+  return x.replace(/[\[\]']+/g, "").split(" ");
+});
+
+const indices = stackInfo[stackInfo.length - 1]
+  .filter((x) => x.match(/[0-9]+/g))
+  .map(Number);
+
+const instructions = input[1].split("\r\n").map((x) => {
+  const [amount, originStack, destinationStack] = x
+    .split(" ")
+    .filter((x) => x.match(/[0-9]+/g));
+  return {
+    amount,
+    originStack,
+    destinationStack,
+  };
+});
+
+console.log(indices);
+
+const createStacks = () => {
+  for (let i = 0; i < indices.length; i++) {}
+};
