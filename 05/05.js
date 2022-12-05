d6bd40b6669236066e956fcb22b5cf9953f89c0e
Elfving Long David (SE) <david.elfvinglong@baesystems.se>
2022-12-05 10:38:10 +0100

    dont have time

--- 05/05.js
+++ 05/05.js
@@ -5,12 +5,12 @@ const input = fs
   .filter((x) => x);
 
 const stackInfo = input[0].split("\r\n").map((x) => {
-  return x.replace(/[\[\]']+/g, "").split(" ");
+  return x.replace(/[\[\]']+/g, "").match(/(\s{4}|\[(\S)\])/g);
 });
 
-const indices = stackInfo[stackInfo.length - 1]
-  .filter((x) => x.match(/[0-9]+/g))
-  .map(Number);
+// const indices = stackInfo[stackInfo.length - 1]
+//   .filter((x) => x.match(/[0-9]+/g))
+//   .map(Number);
 
 const instructions = input[1].split("\r\n").map((x) => {
   const [amount, originStack, destinationStack] = x
@@ -23,8 +23,23 @@ const instructions = input[1].split("\r\n").map((x) => {
   };
 });
 
-console.log(stackInfo, indices, instructions);
+//console.log(stackInfo, indices, instructions);
 
-const createStacks = () => {
-  for (let i = 0; i < indices.length; i++) {}
+const createStacks = (stackInfo, indices) => {
+  let stacks = [];
+  for (let i = 0; i < indices.length - 1; i++) {
+    stacks.push([]);
+  }
+  for (let j = 0; j < stackInfo.length - 1; j++) {
+    for (let k = 0; k < stackInfo[j].length; k++) {
+      if (stackInfo[j][k] !== "") {
+        stacks[k].push(stackInfo[j][k]);
+      }
+    }
+  }
+  console.log(stacks);
 };
+
+//createStacks(stackInfo, indices);
+
+console.log(stackInfo);
