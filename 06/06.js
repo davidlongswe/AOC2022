bb826f64650a41ed900acfab3c0ad6e30278e974
Dave <davidlongswe@gmail.com>
2022-12-06 07:01:35 +0100

    day 6 complete

--- 06/06.js
+++ 06/06.js
@@ -1,5 +1,32 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync("06.txt", { encoding: "utf-8" })
-  .split("\n")
-  .filter((x) => x);
+const characters = fs.readFileSync("06.txt", { encoding: "utf-8" }).split("");
+
+function duplicateExists(arr) {
+  return new Set(arr).size !== arr.length;
+}
+
+const buffer = {
+  charsChecked: 0,
+  currArr: [],
+};
+
+const findMessageLoc = (messageLength) => {
+  for (let c of characters) {
+    buffer.currArr.push(c);
+    buffer.charsChecked++;
+    if (buffer.currArr.length === messageLength) {
+      if (duplicateExists(buffer.currArr)) {
+        buffer.currArr.shift();
+      } else {
+        break;
+      }
+    }
+  }
+  console.log(buffer.charsChecked);
+  buffer.charsChecked = 0;
+};
+
+//part 1
+findMessageLoc(4);
+//part 2
+findMessageLoc(14);
