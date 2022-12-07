8eb5f443a9842c8e310864a790ca0b2c9ce332c8
Elfving Long David (SE) <david.elfvinglong@baesystems.se>
2022-12-07 15:21:59 +0100

    refactored

--- 07/07.js
+++ 07/07.js
@@ -1,48 +1,49 @@
 const fs = require("fs");
-const input = fs
+const args = fs
   .readFileSync("07.txt", { encoding: "utf-8" })
   .split("\r\n")
   .map((x) => x);
 
-const getDirectories = (arr) => {
-  const directories = new Map();
-  let visitedDirectories = [];
-  let currentDirectory = "";
+const getDirectoryTree = (args) => {
+  const directories = {};
+  const parserPath = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let command = arr[i].split(" ");
-
-    if (command[0] === "$" && command[1] === "cd") {
-      if (command[2] === "..") {
-        visitedDirectories.pop();
-        currentDirectory = visitedDirectories[visitedDirectories.length - 1];
-      } else {
-        currentDirectory = command[2];
-        if (!directories.has(currentDirectory))
-          directories.set(currentDirectory, []);
-        visitedDirectories.push(currentDirectory);
-      }
-    } else if (/^\d+$/.test(command[0])) {
-      for (let visitedDirectory of visitedDirectories) {
-        directories.get(visitedDirectory).push(parseInt(command[0]));
-      }
+  for (let c of args) {
+    let [prompt, command, params] = c.split(" ");
+    if (prompt === "$" && command === "cd") {
+      params === ".." ? parserPath.pop() : parserPath.push(params);
+    } else if (/^\d+$/.test(prompt)) {
+      const fileSize = Number(prompt.match(/\d+/)[0]);
+      const path = [];
+      parserPath.forEach((dir) => {
+        path.push(dir);
+        const dirTotal = directories[path.join("/")] ?? 0;
+        directories[path.join("/")] = dirTotal + fileSize;
+      });
     }
   }
-
   return directories;
 };
 
-const dirs = getDirectories(input);
-console.log(dirs);
+const dirTree = getDirectoryTree(args);
 
 const part1 = (directories) => {
-  return [...directories.values()]
-    .filter((directory) => directory.reduce((a, b) => a + b, 0) <= 100000)
-    .flat()
+  //1243729
+  return Object.values(directories)
+    .filter((dirSize) => dirSize <= 1e5)
     .reduce((a, b) => a + b, 0);
 };
 
-const part2 = () => {};
+const part2 = (directories) => {
+  //4443914
+  const AVAILABLE_SPACE = 70000000;
+  const NEEDED_SPACE = 30000000;
+  const ROOT_DIR_SIZE = Object.values(directories).sort((a, b) => b - a)[0];
+  const UNUSED_SPACE = AVAILABLE_SPACE - ROOT_DIR_SIZE;
+  return Object.values(directories)
+    .filter((dirSize) => UNUSED_SPACE + dirSize >= NEEDED_SPACE)
+    .sort((a, b) => a - b)[0];
+};
 
-console.log(part1(dirs));
-part2();
+console.log(part1(dirTree));
+console.log(part2(dirTree));
