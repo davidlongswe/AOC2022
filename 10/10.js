d237c6ebf48a6e95f77bc18dd54ea2e7a0245c59
Dave <davidlongswe@gmail.com>
2022-12-10 13:36:59 +0100

    day 10 complete

--- 10/10.js
+++ 10/10.js
@@ -1,6 +1,55 @@
 const fs = require("fs");
+const instructions = fs
+  .readFileSync("10.txt", { encoding: "utf-8" })
+  .split("\r\n");
 
-const input = fs
-  .readFileSync("0?.txt", { encoding: "utf-8" })
-  .split("\r\n")
-  .map((x) => x);
+const FREQ = [20, 60, 100, 140, 180, 220];
+
+let x = 1;
+let totCycles = 0;
+let part1Res = 0;
+
+let CRTx = -1;
+let CRT = [];
+let CRTLine = "";
+
+instructions.forEach((instruction) => {
+  let [command, amount] = instruction.split(" ");
+  totCycles++;
+  CRTx++;
+  x == CRTx || x == CRTx - 1 || x == CRTx + 1
+    ? (CRTLine += "#")
+    : (CRTLine += " ");
+
+  if (CRTx == 39) {
+    CRT.push(CRTLine);
+    CRTLine = "";
+    CRTx = -1;
+  }
+
+  if (FREQ.includes(totCycles)) partOne += x * totCycles;
+
+  if (amount) {
+    amount = parseInt(amount);
+    totCycles++;
+    CRTx++;
+    x == CRTx || x == CRTx - 1 || x == CRTx + 1
+      ? (CRTLine += "#")
+      : (CRTLine += " ");
+
+    if (CRTx == 39) {
+      CRT.push(CRTLine);
+      CRTLine = "";
+      CRTx = -1;
+    }
+
+    if (FREQ.includes(totCycles)) partOne += x * totCycles;
+    x += amount;
+  }
+});
+
+console.log("Part one = " + part1Res);
+
+CRT.forEach((sym) => {
+  console.log(sym);
+});
