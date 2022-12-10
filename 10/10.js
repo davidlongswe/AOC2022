8f5c565220546bc3899c0bca1a4a4897b2b14912
Dave <davidlongswe@gmail.com>
2022-12-10 15:16:11 +0100

    refactored

--- 10/10.js
+++ 10/10.js
@@ -3,35 +3,33 @@ const instructions = fs
   .readFileSync("10.txt", { encoding: "utf-8" })
   .split("\r\n");
 
-const FREQ = [20, 60, 100, 140, 180, 220];
+const FREQENCIES = [20, 60, 100, 140, 180, 220];
 
-let x = 1;
-let totCycles = 0;
-let part1Res = 0;
-
-let CRTx = -1;
-let CRT = [];
-let CRTLine = "";
-
-instructions.forEach((instruction) => {
-  let [command, amount] = instruction.split(" ");
-  totCycles++;
-  CRTx++;
-  x == CRTx || x == CRTx - 1 || x == CRTx + 1
-    ? (CRTLine += "#")
-    : (CRTLine += " ");
-
-  if (CRTx == 39) {
-    CRT.push(CRTLine);
-    CRTLine = "";
-    CRTx = -1;
-  }
-
-  if (FREQ.includes(totCycles)) partOne += x * totCycles;
-
-  if (amount) {
-    amount = parseInt(amount);
+const part1 = () => {
+  let x = 1;
+  let totCycles = 0;
+  let part1Res = 0;
+  instructions.forEach((instruction) => {
+    let [_, amount] = instruction.split(" ").map(Number);
     totCycles++;
+    if (FREQENCIES.includes(totCycles)) part1Res += x * totCycles;
+
+    if (amount) {
+      totCycles++;
+      if (FREQENCIES.includes(totCycles)) part1Res += x * totCycles;
+      x += amount;
+    }
+  });
+  console.log("Part one: " + part1Res);
+};
+
+const part2 = () => {
+  let x = 1;
+  let CRTx = -1;
+  let CRT = [];
+  let CRTLine = "";
+  instructions.forEach((instruction) => {
+    let [_, amount] = instruction.split(" ");
     CRTx++;
     x == CRTx || x == CRTx - 1 || x == CRTx + 1
       ? (CRTLine += "#")
@@ -42,14 +40,27 @@ instructions.forEach((instruction) => {
       CRTLine = "";
       CRTx = -1;
     }
+    if (amount) {
+      amount = parseInt(amount);
 
-    if (FREQ.includes(totCycles)) partOne += x * totCycles;
-    x += amount;
-  }
-});
+      CRTx++;
+      x == CRTx || x == CRTx - 1 || x == CRTx + 1
+        ? (CRTLine += "#")
+        : (CRTLine += " ");
 
-console.log("Part one = " + part1Res);
+      if (CRTx == 39) {
+        CRT.push(CRTLine);
+        CRTLine = "";
+        CRTx = -1;
+      }
+      x += amount;
+    }
+  });
+  CRT.forEach((sym) => {
+    console.log(sym);
+  });
+};
 
-CRT.forEach((sym) => {
-  console.log(sym);
-});
+part1();
+
+part2();
