7350f41ede41b21a2724629289f99690057212a2
Elfving Long David (SE) <david.elfvinglong@baesystems.se>
2022-12-14 17:19:08 +0100

    progress?

--- 14/14.js
+++ 14/14.js
@@ -1,94 +1,87 @@
-const { FORMERR } = require("dns");
 const fs = require("fs");
 
 const caveScan = fs
   .readFileSync("14.txt", { encoding: "utf-8" })
   .split("\r\n")
-  .map((x) =>
-    x
+  .map((row) =>
+    row
       .split(" -> ")
-      .map((x) => x.split(","))
-      .map((x) => {
+      .map((coords) => coords.split(","))
+      .map((coord) => {
         return {
-          xCoord: parseInt(x[0]),
-          yCoord: parseInt(x[1]),
+          xCoord: parseInt(coord[0]),
+          yCoord: parseInt(coord[1]),
         };
       })
   );
 
-const getMinXCoord = () => {
-  return caveScan
-    .flat()
-    .map((x) => x.xCoord)
-    .sort((a, b) => a - b)[0];
-};
-const getMaxXCoord = () => {
-  return caveScan
-    .flat()
-    .map((x) => x.xCoord)
-    .sort((a, b) => b - a)[0];
-};
+class CaveObject {
+  constructor(x, y, sym) {
+    this.x = x;
+    this.y = y;
+    this.sym = sym;
+  }
+}
 
-const getMinYCoord = () => {
-  return caveScan
-    .flat()
-    .map((x) => x.yCoord)
-    .sort((a, b) => a - b)[0];
-};
-
-const getMaxYCoord = () => {
-  return caveScan
-    .flat()
-    .map((x) => x.yCoord)
-    .sort((a, b) => b - a)[0];
-};
-
-const drawObjectsOnCaveSlice = (caveSlice) => {
-  caveSlice[0][500 - getMinXCoord()] = "+";
-  for (let row of caveScan) {
-    for (let i = 0; i < row.length; i++) {
-      let xOrig = row[i].xCoord;
-      let xDest = row[i + 1]?.xCoord;
-      let yOrig = row[i].yCoord;
-      let yDest = row[i + 1]?.yCoord;
+const placeObjectsInCave = (cave) => {
+  for (let r of caveScan) {
+    for (let i = 0; i < r.length - 1; i++) {
+      let xOrig = r[i].xCoord;
+      let yOrig = r[i].yCoord;
+      let xDest = r[i + 1]?.xCoord;
+      let yDest = r[i + 1]?.yCoord;
       if (xOrig === xDest) {
-        for (let j = 0; j < yDest - yOrig; j++) {
-          console.log(i, j);
-          caveSlice[yOrig - getMinYCoord() + j + 1][xOrig - getMinXCoord()] =
-            "#";
+        for (let j = yOrig; j < yDest + 1; j++) {
+          let caveObj;
+          for (let row in cave) {
+            let tempCaveObj = row.find((obj) => obj.x === xOrig && obj.y === j);
+            if (tempCaveObj) {
+              caveObj = tempCaveObj;
+            }
+          }
+          console.log(caveObj);
+          //caveObj.sym = "#";
         }
       }
       if (yOrig === yDest) {
-        for (let j = 0; j < xDest - xOrig; j++) {
-          console.log(i, j);
-          //console.log(xDest, xOrig);
-          caveSlice[yOrig - getMinYCoord()][xOrig - getMinXCoord() + j + 1] =
-            "#";
+        let start = xOrig < xDest ? xOrig : xDest;
+        let end = xDest > xOrig ? xDest : xOrig;
+        for (let k = start; k < end + 1; k++) {
+          let caveObj = cave[i].find((obj) => obj.y === yOrig && obj.x === k);
+          //console.log("i:" + i, k, yOrig, caveObj);
+          //caveObj.sym = "#";
         }
       }
     }
   }
+  return cave;
 };
 
-const createCaveSlice = () => {
-  const caveSliceWidth = getMaxXCoord() - getMinXCoord();
-  const caveSliceDepth = getMaxYCoord();
-  let caveSlice = [];
-  for (let i = 0; i < caveSliceDepth; i++) {
-    let caveSliceRow = [];
-    for (let j = 0; j < caveSliceWidth; j++) {
-      caveSliceRow.push(".");
+const createCave = () => {
+  let xCoords = caveScan.flat().map((x) => x.xCoord);
+  let yCoords = caveScan.flat().map((x) => x.yCoord);
+  let xMin = xCoords.sort((a, b) => a - b)[0];
+  let xMax = xCoords.sort((a, b) => b - a)[0];
+  let yMin = yCoords.sort((a, b) => a - b)[0];
+  let yMax = yCoords.sort((a, b) => b - a)[0];
+
+  let cave = [];
+
+  for (let i = yMin; i < yMax + 1; i++) {
+    let caveRow = [];
+    for (let j = xMin; j < xMax + 1; j++) {
+      caveRow.push(new CaveObject(j, i, "."));
     }
-    caveSlice.push(caveSliceRow);
+    cave.push(caveRow);
   }
 
-  drawObjectsOnCaveSlice(caveSlice);
+  let caveWithObjs = placeObjectsInCave(cave);
 
-  return caveSlice;
+  return caveWithObjs;
 };
 
 const simulateSand = () => {};
 
-const caveSlice = createCaveSlice();
+const cave = createCave();
 
-console.log(caveSlice);
+console.log(cave);
